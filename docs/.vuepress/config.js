82eab37fa36efb0fccb1136dafadad6ffa6bb52f
msh01 <yingwaizhiying@gmail.com>
2020-02-16 17:38:38 +0800

    refactor(项目骨架): 左侧导航栏问题已解决；faver icon未解决

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,10 +4,24 @@
  * @date 2020/2/14 23:10.
  */
 module.exports = {
-    base: '/elements-of-ai-zh',
+    base: '/elements-of-ai-zh/',
     title: 'AI 基础入门教程',
     description: 'AI 基础入门教程',
+    head:
+        [
+            ['link', {rel: 'icon', href: '/logo/small.png'}],
+            ['script', {}, `
+            var _hmt = _hmt || [];
+            (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?9dcf4bca9cc47caeef4caf5d5f982e86";
+                var s = document.getElementsByTagName("script")[0]; 
+                s.parentNode.insertBefore(hm, s);
+            })();
+            `]
+        ],
     themeConfig: {
+        logo: '/logo/small.png',
         nav: [
             {
                 text: 'Home',
@@ -25,7 +39,7 @@ module.exports = {
                 collapsable: false, // 可选的, 默认值是 true,
                 sidebarDepth: 2,    // 可选的, 默认值是 1
                 children: [
-                    '1','2'
+                    "/what-is-ai/1", "/what-is-ai/2"
                 ]
             },
             {
@@ -34,38 +48,55 @@ module.exports = {
                 collapsable: false, // 可选的, 默认值是 true,
                 sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: [
-                    '3','4'
+                    '/real-world-ai/3', '/real-world-ai/4'
+                ]
+            },
+            {
+                title: '捐赠',
+                // path: '/donation/',
+                collapsable: false,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    '/what-is-ai/juanzeng'
                 ]
             }
         ],
-
+        '/donation/': [
+            '/donation/',
+        ],
+        '/version/': [
+            'V2.4',
+        ],
         repo: 'https://github.com/msh01/elements-of-ai-zh.git',
-        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: '查看源码',
-        // 以下为可选的编辑链接选项
-        // 假如你的文档仓库和项目本身不在一个仓库：
-        // docsRepo: 'vuejs/vuepress',
-        // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
-        // 假如文档放在一个特定的分支下：
-        docsBranch: 'master',
-        // 默认是 false, 设置为 true 来启用
-        editLinks: true,
-        // 默认为 "Edit this page"
+        // // 假如你的文档仓库和项目本身不在一个仓库：
+        // // docsRepo: 'vuejs/vuepress',
+        // // 假如文档不是放在仓库的根目录下：
+        // docsDir: 'docs',
+        // // 假如文档放在一个特定的分支下：
+        // docsBranch: 'master',
+        // // 默认是 false, 设置为 true 来启用
+        // editLinks: true,
+        // // 默认为 "Edit this page"
         editLinkText: '在线修订此页面',
-        lastUpdated: '修订于', // string | boolean
+        lastUpdated: '修订于'// string | boolean
     },
     // 插件设置
     plugins: [
-        '@limdongjin/vuepress-plugin-sidebar-on-off',
-        '@vuepress/back-to-top',
         [
-            '@vuepress/container',
+            'vuepress-plugin-comment',
             {
-                type: 'slot'
+                choosen: 'valine',
+                // options选项中的所有参数，会传给Valine的配置
+                options: {
+                    el: '#valine-vuepress-comment',
+                    appId: 'slnTcOTAPQVydGNesVApRUcg-gzGzoHsz',
+                    appKey: 'Aeusc62FpStk4K4yVAiFonnU'
+                }
             }
-        ]
+        ],
+        '@limdongjin/vuepress-plugin-sidebar-on-off',
+        '@vuepress/back-to-top'
     ],
     markdown: {
         extendMarkdown: md => {
---
+++ docs/donation/README.md
@@ -0,0 +1,6 @@
+### 捐赠支持
+> 如果该项目帮到你了，可以请作者喝杯咖啡 ☕ ！
+
+|   微信  |   支付宝  |
+|--- | --- |
+|  ![](https://raw.githubusercontent.com/msh01/PicGo/master/mm_facetoface_collect_qrcode_1518083664577.png)   |  ![](https://raw.githubusercontent.com/msh01/PicGo/master/1518083716442.jpg)  |
